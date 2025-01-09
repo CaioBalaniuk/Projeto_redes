663eec103465474d5d58397da01ba61c88a3ea9f
Caio Medeiros Balaniuk <135898371+CaioBalaniuk@users.noreply.github.com>
2025-01-09 01:17:47 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,15 +1,1 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import StartPage from "./pages/startPage";  // Certifique-se de que o caminho está correto
-import NamePage from "./pages/namePage";
 
-function App() {
-    return (
-        <Routes>
-          <Route path="/" element={<StartPage />} />
-          <Route path="/NamePage" element={<NamePage />} />
-        </Routes>
-    );
-}
-
-export default App;
